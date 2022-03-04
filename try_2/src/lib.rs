139a7cf03167d47d835df279a1d536eb7442df33
Supdrewin <supdrewin@outlook.com>
2022-03-04 12:33:10 +0000

    Starting `try_2`.

--- try_2/src/lib.rs
+++ try_2/src/lib.rs
@@ -1,2 +1,71 @@
+use std::mem;
+
+pub struct Solution;
+
+impl Solution {
+    /// Question 1
+    ///
+    /// Enter 1..=12 into pentagram, except 7 and 11.
+    pub fn pentagram_game() -> Vec<i32> {
+        todo!()
+    }
+
+    /// Question 2 - Red and Black
+    ///
+    /// '.' is a black block, '#' is a red block, '@' is a human. The human can only
+    /// move between the black blocks. This method use a simple `DFS` algorithm, but
+    /// it's sloooooowly.
+    pub fn red_and_black(room: &Vec<Vec<char>>, pos: (usize, usize)) -> usize {
+        let mut result = 1;
+        let mut goto = |pos: (usize, usize)| {
+            let mut room = room.clone();
+            if let Some(vec) = room.get_mut(pos.0) {
+                if let Some(c) = vec.get_mut(pos.1) {
+                    if mem::replace(c, '#') == '.' {
+                        result = result.max(Self::red_and_black(&room, pos) + 1);
+                    }
+                }
+            }
+        };
+        if pos.0 > 0 {
+            goto((pos.0 - 1, pos.1));
+        }
+        if pos.1 > 0 {
+            goto((pos.0, pos.1 - 1));
+        }
+        goto((pos.0 + 1, pos.1));
+        goto((pos.0, pos.1 + 1));
+        result
+    }
+
+    /// Question 3
+    ///
+    ///
+    pub fn sliding_puzzle() {
+        todo!()
+    }
+
+    /// Get the inverse number of a board.
+    ///
+    /// This method is mainly for verifying whether a board
+    /// is valid.
+    fn _inverse_number() {
+        todo!()
+    }
+
+    /// Question 4
+    ///
+    /// Write an algorithm to print all ways of arranging n
+    /// queens on an n x n chess board so that none of them
+    /// share the same row, column, or diagonal. In this case,
+    /// "diagonal" means all diagonals, not just the two that
+    /// bisect the board.
+    ///
+    /// https://leetcode-cn.com/problems/eight-queens-lcci
+    pub fn solve_n_queens() {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests;
--- try_2/src/tests.rs
+++ try_2/src/tests.rs
@@ -1,1 +1,35 @@
+use crate::Solution;
 
+#[test]
+fn pentagram_game() {
+    let result = Solution::pentagram_game();
+    println!("{:?}", result);
+}
+
+#[test]
+fn red_and_black() {
+    let room = vec![
+        vec!['.', '.', '.', '.', '#'],
+        vec!['.', '.', '.', '.', '.'],
+        vec!['#', '@', '.', '.', '.'],
+        vec!['.', '#', '.', '.', '#'],
+    ];
+    for vec in &room {
+        println!("{:?}", vec);
+    }
+    let mut pos = (0, 0);
+    for (x, vec) in room.iter().enumerate() {
+        for (y, &c) in vec.iter().enumerate() {
+            if c == '@' {
+                pos = (x, y);
+            }
+        }
+    }
+    assert_eq!(Solution::red_and_black(&room, pos), 15);
+}
+
+#[test]
+fn sliding_puzzle() {}
+
+#[test]
+fn solve_n_queens() {}
