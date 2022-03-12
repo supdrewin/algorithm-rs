8fa9dcd3f3f38b264b72679ba105ec38b33e3014
Supdrewin <supdrewin@outlook.com>
2022-03-12 10:55:41 +0000

    improve sort and tests.

--- sort/src/lib.rs
+++ sort/src/lib.rs
@@ -1,4 +1,4 @@
-use std::mem;
+#![feature(is_sorted)]
 
 pub struct Solution;
 
@@ -6,87 +6,89 @@ impl Solution {
     /// Quick Sort Algorithm
     ///
     /// TODO: Add comments.
-    pub fn quick_sort<F, T>(vec: &mut Vec<T>, cmp: F)
+    pub fn quick_sort<F, T>(src: &Vec<T>, cmp: F) -> Vec<T>
     where
         F: Copy + FnMut(&T, &T) -> bool,
+        T: Clone,
     {
-        fn sort<F, T>(vec: &mut Vec<T>, mut cmp: F, (left, right): (usize, usize))
+        fn sort<F, T>(dist: &mut Vec<T>, mut cmp: F, (left, right): (usize, usize))
         where
             F: Copy + FnMut(&T, &T) -> bool,
+            T: Clone,
         {
             let (mut idx1, mut idx2) = (left, right);
             while idx1 < idx2 {
-                while idx1 < idx2 && cmp(&vec[idx1], &vec[idx2]) {
+                while idx1 < idx2 && cmp(&dist[idx1], &dist[idx2]) {
                     idx1 += 1;
                 }
                 if idx1 < idx2 {
-                    vec.swap(idx1, idx2);
+                    dist.swap(idx1, idx2);
                     idx2 -= 1;
                 }
-                while idx1 < idx2 && cmp(&vec[idx1], &vec[idx2]) {
+                while idx1 < idx2 && cmp(&dist[idx1], &dist[idx2]) {
                     idx2 -= 1;
                 }
                 if idx1 < idx2 {
-                    vec.swap(idx1, idx2);
+                    dist.swap(idx1, idx2);
                     idx1 += 1;
                 }
             }
             if left + 1 < idx2 {
-                sort(vec, cmp, (left, idx2 - 1));
+                sort(dist, cmp, (left, idx2 - 1));
             }
             if idx1 + 1 < right {
-                sort(vec, cmp, (idx1 + 1, right));
+                sort(dist, cmp, (idx1 + 1, right));
             }
         }
-        assert!(vec.len() > 0);
-        sort(vec, cmp, (0, vec.len() - 1));
+        let mut dist = src.clone();
+        (src.len() > 0).then(|| sort(&mut dist, cmp, (0, src.len() - 1)));
+        dist
     }
 
     /// Merge Sort Algorithm
     ///
     /// TODO: Add comments.
-    pub fn merge_sort<F, T>(vec: &mut Vec<T>, cmp: F)
+    pub fn merge_sort<F, T>(src: &Vec<T>, cmp: F) -> Vec<T>
     where
         F: Copy + FnMut(&T, &T) -> bool,
         T: Clone,
     {
-        fn sort<F, T>((slice1, slice2): (&[T], &[T]), mut cmp: F) -> Vec<T>
+        fn sort<F, T>((part1, part2): (&[T], &[T]), mut cmp: F) -> Vec<T>
         where
             F: Copy + FnMut(&T, &T) -> bool,
             T: Clone,
         {
-            let sort = |slice: &[T]| {
-                if slice.len() > 1 {
-                    sort(slice.split_at(slice.len() >> 1), cmp)
+            let sort = |part: &[T]| {
+                if part.len() > 1 {
+                    sort(part.split_at(part.len() >> 1), cmp)
                 } else {
-                    slice.iter().map(|val| val.clone()).collect::<Vec<T>>()
+                    part.iter().map(|val| val.clone()).collect::<Vec<T>>()
                 }
             };
-            let vec1 = sort(slice1);
-            let vec2 = sort(slice2);
-            let mut dist = Vec::new();
+            let part1 = sort(part1);
+            let part2 = sort(part2);
+            let mut dist = Vec::with_capacity(part1.len() + part2.len());
             let mut take = |src: &Vec<T>, idx: &mut usize| {
                 dist.push(src[*idx].clone());
                 *idx += 1;
             };
-            let (mut idx1, mut idx2) = Default::default();
-            while idx1 < vec1.len() && idx2 < vec2.len() {
-                if cmp(&vec1[idx1], &vec2[idx2]) {
-                    take(&vec1, &mut idx1);
+            let (mut idx1, mut idx2) = (0, 0);
+            while idx1 < part1.len() && idx2 < part2.len() {
+                if cmp(&part1[idx1], &part2[idx2]) {
+                    take(&part1, &mut idx1);
                 } else {
-                    take(&vec2, &mut idx2);
+                    take(&part2, &mut idx2);
                 }
             }
-            let mut cnt = |vec: &Vec<T>, idx: &mut usize| {
-                while *idx < vec.len() {
-                    take(vec, idx);
-                }
-            };
-            cnt(&vec1, &mut idx1);
-            cnt(&vec2, &mut idx2);
+            while idx1 < part1.len() {
+                take(&part1, &mut idx1);
+            }
+            while idx2 < part2.len() {
+                take(&part2, &mut idx2);
+            }
             dist
         }
-        mem::drop(mem::replace(vec, sort(vec.split_at(vec.len() >> 1), cmp)));
+        sort(src.split_at(src.len() >> 1), cmp)
     }
 }
 
--- sort/src/tests.rs
+++ sort/src/tests.rs
@@ -3,56 +3,60 @@ use rand::prelude::*;
 
 #[test]
 fn quick_sort_1() {
-    let mut src = (0..10_000).into_iter().collect::<Vec<usize>>();
-    let dist = (0..10_000).into_iter().collect::<Vec<usize>>();
-    Solution::quick_sort(&mut src, |a, b| a < b);
-    assert_eq!(src, dist);
+    assert!(Solution::quick_sort(
+        &(-10_000..10_000).into_iter().collect::<Vec<i16>>(),
+        |a, b| a < b
+    )
+    .is_sorted());
 }
 
 #[test]
 fn quick_sort_2() {
-    let mut src = (0..10_000).into_iter().collect::<Vec<usize>>();
-    let dist = (0..10_000).rev().into_iter().collect::<Vec<usize>>();
-    Solution::quick_sort(&mut src, |a, b| a > b);
-    assert_eq!(src, dist);
+    assert!(Solution::quick_sort(
+        &(-10_000..10_000).into_iter().collect::<Vec<i16>>(),
+        |a, b| a > b
+    )
+    .is_sorted_by(|a, b| b.partial_cmp(a)));
 }
 
 #[test]
 fn quick_sort_3() {
-    let mut src = (0..1_000_000)
-        .into_iter()
-        .map(|_| thread_rng().gen_range(0..1_000_000))
-        .collect::<Vec<usize>>();
-    let mut dist = src.clone();
-    Solution::quick_sort(&mut src, |a, b| a < b);
-    dist.sort();
-    assert_eq!(src, dist);
+    assert!(Solution::quick_sort(
+        &(0..2_000_000)
+            .into_iter()
+            .map(|_| thread_rng().gen_range(-1_000_000..1_000_000))
+            .collect::<Vec<i32>>(),
+        |a, b| a < b
+    )
+    .is_sorted());
 }
 
 #[test]
 fn merge_sort_1() {
-    let mut src = (0..1_000_000).into_iter().collect::<Vec<usize>>();
-    let dist = (0..1_000_000).into_iter().collect::<Vec<usize>>();
-    Solution::merge_sort(&mut src, |a, b| a < b);
-    assert_eq!(src, dist);
+    assert!(Solution::merge_sort(
+        &(-1_000_000..1_000_000).into_iter().collect::<Vec<i32>>(),
+        |a, b| a < b,
+    )
+    .is_sorted());
 }
 
 #[test]
 fn merge_sort_2() {
-    let mut src = (0..1_000_000).into_iter().collect::<Vec<usize>>();
-    let dist = (0..1_000_000).rev().into_iter().collect::<Vec<usize>>();
-    Solution::merge_sort(&mut src, |a, b| a > b);
-    assert_eq!(src, dist);
+    assert!(Solution::merge_sort(
+        &(-1_000_000..1_000_000).into_iter().collect::<Vec<i32>>(),
+        |a, b| a > b,
+    )
+    .is_sorted_by(|a, b| b.partial_cmp(a)));
 }
 
 #[test]
 fn merge_sort_3() {
-    let mut src = (0..1_000_000)
-        .into_iter()
-        .map(|_| thread_rng().gen_range(0..1_000_000))
-        .collect::<Vec<usize>>();
-    let mut dist = src.clone();
-    Solution::merge_sort(&mut src, |a, b| a < b);
-    dist.sort();
-    assert_eq!(src, dist);
+    assert!(Solution::merge_sort(
+        &(0..2_000_000)
+            .into_iter()
+            .map(|_| thread_rng().gen_range(-1_000_000..1_000_000))
+            .collect::<Vec<i32>>(),
+        |a, b| a < b
+    )
+    .is_sorted());
 }
