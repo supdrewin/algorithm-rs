09afd48cbd8c106f0eee7ed9131f61a8bb10d678
Supdrewin <supdrewin@outlook.com>
2022-03-17 16:08:22 +0000

    Update `Docs`.

--- docs/source-files.js
+++ docs/source-files.js
@@ -6,4 +6,5 @@ sourcesIndex["treap"] = {"name":"","files":["lib.rs","map.rs","node.rs","set.rs"
 sourcesIndex["try_1"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["try_2"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["try_3"] = {"name":"","files":["btree.rs","lib.rs"]};
+sourcesIndex["try_4"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
